a206efc2abdca6f445f382529d236d99938f5d7e
Ivan Tivonenko <darkdarkdragon@gmail.com>
2015-02-20 05:10:16 +0200

    added cordova project template

---
+++ src/project/run.cmd
@@ -0,0 +1,4 @@
+::set project="file://D:\Users\dark\Projects\ripple\Ripple-Android-Wallet\app\platforms\browser\www\index.html
+set project="file:///D:/Users/dark/Projects/ripple/work/ripple-wallet-mobile/src/project/platforms/browser/www/index.html"
+set chrome=D:\dev\tools\chrome-win32\chrome.exe
+%chrome% --disable-web-security  --user-data-dir=d:\Chromedevsession %project%
